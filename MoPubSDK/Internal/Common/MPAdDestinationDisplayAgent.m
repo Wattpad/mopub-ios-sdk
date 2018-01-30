be39dc9d7d7ec2983be26c0551eb9691dc9a5446
Elton Gao <gyfelton@gmail.com>
2018-01-30 11:03:38 -0500

    Make sure vc are properly deallocated after dismiss

--- MoPubSDK/Internal/Common/MPAdDestinationDisplayAgent.m
+++ MoPubSDK/Internal/Common/MPAdDestinationDisplayAgent.m
@@ -324,6 +324,10 @@ static NSString * const kDisplayAgentErrorDomain = @"com.mopub.displayagent";
 {
     self.isLoadingDestination = NO;
     [self hideModalAndNotifyDelegate];
+    // Added by Elton: make sure vc is properly deallocated after dismiss
+    if (viewController == self.storeKitController) {
+        self.storeKitController = nil;
+    }
 }
 
 #pragma mark - <MPAdBrowserControllerDelegate>
@@ -332,6 +336,10 @@ static NSString * const kDisplayAgentErrorDomain = @"com.mopub.displayagent";
 {
     self.isLoadingDestination = NO;
     [self hideModalAndNotifyDelegate];
+    // Added by Elton: make sure vc is properly deallocated after dismiss
+    if (browserController == self.browserController) {
+        self.browserController = nil;
+    }
 }
 
 - (MPAdConfiguration *)adConfiguration
